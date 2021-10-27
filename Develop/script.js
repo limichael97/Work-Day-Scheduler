53e612ed4380147e47311318905f01e9283e0b5e
Michael Li <thisislimichael@gmail.com>
2021-10-27 12:27:22 -0700

    explained code

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,20 @@
-$(document).ready(function (){
+
+
+// create array to hold times
+
     timeArray = ["9", "10", "11", "12", "13", "14", "15", "16", "17",];
     var parent = $("#parent");
 
-
-function showTime() {
+// function to display the current date/time on page
+    function showTime() {
     var time = moment().format('MMMM Do YYYY, h:mm:ss a')
     var timeP = $("<p>").text(time);
     $("#currentDay").append(timeP);
-}
-showTime()
+    }
+    showTime()
 
 
-
+// function to dynamically make the timeblocks from 9am-5pm
 for (i=0; i<timeArray.length; i++) {
     var element1 = document.createElement("div");
     var element2 = document.createElement("div");
@@ -28,7 +31,7 @@ for (i=0; i<timeArray.length; i++) {
     $(element4).addClass("btn saveBtn col-md-1");
     $(element4).html("<i class='fas fa-save'></i>")
 
-    
+    // converting 24h time 
     if (timeArray[i]%12 == 0) {
         $(element2).text(`12pm`);
     } 
@@ -41,8 +44,10 @@ for (i=0; i<timeArray.length; i++) {
         $(element2).text(`${timeArray[i]%12}am`);
     }
 
+    // get local storage
     $(`#hour-${timeArray[i]} .description`).val(localStorage.getItem(`hour-${timeArray[i]}`))
 
+    // change background colors depending on if time passed or in the future
     if (timeArray[i] < moment().hours()) {
         $(element3).css("background-color", "red");
     }
@@ -56,6 +61,7 @@ for (i=0; i<timeArray.length; i++) {
     }
 }
 
+// save to localStorage
 $(".saveBtn").on("click", function() {
     var value = $(this).siblings(".description" ).val();
     var time = $(this).parent().attr("id");
@@ -63,6 +69,6 @@ $(".saveBtn").on("click", function() {
     localStorage.setItem(time, value);
 })
 
-console.log(moment().hours())
 
-})
+
+
